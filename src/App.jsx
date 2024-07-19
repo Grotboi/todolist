5ced485d810c163fb7362a04174bf5d839d857bc
Ilya Pavlov <grotgrot704@gmail.com>
2024-07-19 22:16:29 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import img from './free-icon-bin-484611 (1).png'
 
+//[{ text:'Text Array'}]
 function App() {
 
-  const bd = "https://api.npoint.io/0b62343eaf6f9993f4e2"
+  const [state, setState] = useState(JSON.parse(localStorage.getItem('save')) || [{text: 'Тренеровка 5:30'}])
 
-  const [state, setState] = useState([])
+  const onClickAdd = () => {
+    const text = prompt('Добавить Задачу');
+    setState([...state, {text}]);
+    localStorage.setItem('save', JSON.stringify([...state, {text}]));
+  
+  }
 
-  useEffect(() => {
+  const onClickEdit = (index) => {
+const text = prompt('Изменить Задачу');
+setState(
+  state.map((obj, i) => {
+    if(index === i) {
+      obj.text = text;
+    }
+    localStorage.setItem('save', JSON.stringify([...state, {text}]));
+    return obj;
+  })
+)
+  }
 
-  }, [])
+
+  const onClickRemove = (index) => {
+setState(state.filter((_, i) => i
+!== index))
+localStorage.removeItem('save', JSON.stringify( ...state));
+  }
 
   return (
-    <>
      <div>
-      <input type="text" />
+
+<h1>Список дел</h1>
+      <ul>
+
+        {state.map((task, i) => 
+        <li key={i}>
+          {task.text}
+
+          <button onClick={ () =>
+          onClickEdit(i)}>Изменить</button>
+
+          <button onClick={ () =>
+          onClickRemove(i)}><img src={img} /></button>
+        </li>
+        )}
+
+      </ul>
+
+      <button onClick={
+          onClickAdd}>Добавить</button>
+
      </div>
-    </>
   )
 }
 
